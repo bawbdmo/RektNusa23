b5c9471e7ca5193f1de280dc07487aee3785f0a8
bawbdmo <bawbdmo@gmail.com>
2016-06-22 16:34:36 -0400

    Create viewer.php

---
+++ viewer.php
@@ -0,0 +1,28 @@
+<?php
+if ($_POST["pw"] != null) {
+if ($_POST["pw"] == "232") {
+?><html>
+<style>
+button {
+    background-color: white;
+    border: 2px solid #999999;
+    color: #999999;
+    padding: 15px 15px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-radius: 3px;
+    -webkit-transition-duration: 0.2s; 
+    transition-duration: 0.2s;
+}
+
+button:hover {
+    background-color: #0099ff;
+    color: white;
+    border: 2px solid #0099ff;
+}
+</style></html><?
+echo "<center>" . file_get_contents("Logs.txt") . "</center>";
+}
+}
