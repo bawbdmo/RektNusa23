52282b38280e5bd7cd4a1faa1638a0e4bc1cf3cd
bawbdmo <bawbdmo@gmail.com>
2016-06-22 16:37:29 -0400

    Delete login.php

--- login.php
+++
@@ -1,8 +0,0 @@
-<html>
-<center>
-<form method="post" action="Viewer.php">
-<b>Password:</b> <input type="textbox" name="pw"></input><br><br>
-<input type="submit" value="LOGIN"></input>
-</form>
-</center>
-</html>
