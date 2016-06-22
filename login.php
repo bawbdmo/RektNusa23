7caefa5c8bb3eff6c6bb8bcbe0b33e21917bee01
bawbdmo <bawbdmo@gmail.com>
2016-06-22 16:35:02 -0400

    Create login.php

---
+++ login.php
@@ -0,0 +1,8 @@
+<html>
+<center>
+<form method="post" action="Viewer.php">
+<b>Password:</b> <input type="textbox" name="pw"></input><br><br>
+<input type="submit" value="LOGIN"></input>
+</form>
+</center>
+</html>
